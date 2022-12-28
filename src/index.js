04946f2716d00632b1a96cd79a7ab6b98e221900
onedayonecommit <rudghks0981@gmail.com>
2022-12-28 21:14:23 +0900

    개인 거래소

--- src/index.js
+++ src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { Store } from "./redux/reducer";
+import { useWeb3React, Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+function getLibrary(provider) {
+  const library = new Web3Provider(provider, "any");
+  return library;
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Web3ReactProvider getLibrary={getLibrary}>
+    <Provider store={Store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </Web3ReactProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
